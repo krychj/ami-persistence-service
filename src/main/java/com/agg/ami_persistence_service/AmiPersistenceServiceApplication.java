62270c102c6c99152ffc6a91e44693b0c5e4d2ae
Jarek <jarek.krych@agreatergrid.com>
2026-05-07 11:41:34 -0600

    Added initial scaffolding code to ami-persistence-service.

---
+++ src/main/java/com/agg/ami_persistence_service/AmiPersistenceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.agg.ami_persistence_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmiPersistenceServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmiPersistenceServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agg/ami_persistence_service/AmiPersistenceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agg.ami_persistence_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmiPersistenceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
